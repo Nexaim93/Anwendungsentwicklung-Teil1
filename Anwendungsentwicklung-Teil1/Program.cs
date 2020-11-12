9ae494289eb517da08b7c9e866a33ca8549e75a6
GMSystem <Georgi_93@gmx.de>
2020-11-12 11:54:09 +0100

    test

---
+++ Anwendungsentwicklung-Teil1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anwendungsentwicklung_Teil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
